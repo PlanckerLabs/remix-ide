3a50667e20ef4655db1b007ac53f7c022dd079e8
drafish <xwlyy1991@163.com>
2023-03-22 21:00:40 +0800

    add crowdin spider

---
+++ spider/README.md
@@ -0,0 +1,15 @@
+## Crowdin 爬虫
+安装依赖包
+```
+npm install
+```
+
+登录 Crowdin ，并将 cookie 复制进脚本中
+```
+const cookie = '';
+```
+
+执行命令
+```
+node index.js
+```
---
+++ spider/index.js
@@ -0,0 +1,38 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const cookie = '';
+const file_list_url = 'https://crowdin.com/backend/project/remix-translation/zh-CN/get_files_tree?language_id=55'
+const file_export_url = 'https://crowdin.com/backend/project/remix-translation/zh-CN/{id}/export'
+const response = await fetch(file_list_url);
+const { files_tree } = await response.json();
+
+const file_list = []
+for (const key in files_tree) {
+  const file = files_tree[key];
+  if (file.name.endsWith('.pot')) {
+    file_list.push({ id: file.id, name: file.name.replace('.pot', '.po') })
+  }
+}
+console.log(file_list)
+for (let index = 0; index < file_list.length; index++) {
+  const file = file_list[index];
+  const resp = await fetch(file_export_url.replace('{id}', file.id), { headers: { cookie } });
+  const { url } = await resp.json();
+  const resp2 = await fetch(url, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/octet-stream' }
+  })
+  const arrayBuffer = await resp2.arrayBuffer();
+  fs.writeFile(path.join(__dirname, `../docs/locale/zh_CN/LC_MESSAGES/${file.name}`), Buffer.from(arrayBuffer), "binary", function (err) {
+    if (err) console.error(err);
+    else console.log(`${file.name}下载成功`);
+  });
+}
+
+
+
